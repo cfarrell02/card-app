e2d75177e2f8970a87eb10b80c50edb2a85a7495
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-12-02 12:28:59 +0000

    Initial Commit

---
+++ cardApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cardApp
+//
+//  Created by Cian Farrell on 02/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
